fd5feb788f34cbd1096d63f043a4da2fca3aff6b
pederbr <peder.brennum@gmail.com>
2024-10-17 19:18:43 +0200

    initial commit

---
+++ src/main/java/com/itp/DigLib/DigLibApplication.java
@@ -0,0 +1,13 @@
+package com.itp.DigLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigLibApplication.class, args);
+	}
+
+}
