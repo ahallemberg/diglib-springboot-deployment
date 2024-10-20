a92f142682a754a8ecaa513212c941dfe95d7777
Jørgen Larsen <jorgehla@stud.ntnu.no>
2024-10-20 18:01:22 +0200

    Added avg readingtime to pagedcontent

--- src/main/java/com/itp/DigLib/api/model/PagedContent.java
+++ src/main/java/com/itp/DigLib/api/model/PagedContent.java
@@ -11,6 +11,7 @@ public class PagedContent {
     private final int totalPages;      // Total number of pages available
     private final int pageSize;        // Number of characters in this page
     private final int totalSize;       // Total number of characters in the entire book
+    private final String readingTime;       // Average reading time for the entire book
     
     public PagedContent(String content, int pageNumber, int totalPages, int pageSize, int totalSize) {
         this.content = content;
@@ -18,6 +19,16 @@ public class PagedContent {
         this.totalPages = totalPages;
         this.pageSize = pageSize;
         this.totalSize = totalSize;
+        this.readingTime = calculateReadingTime();
+    }
+
+    private String calculateReadingTime() {
+        int words = this.totalSize / 5; // Average word length is 5 characters
+        int avgReadingTime = 183; // Average reading speed in words per minute
+        int totaltMinutes = words / avgReadingTime;
+        int hours = totaltMinutes / 60;
+        int minutes = totaltMinutes % 60;
+        return String.format("%d hours, %d minutes", hours, minutes);
     }
     
     // Getters
@@ -39,5 +50,9 @@ public class PagedContent {
     
     public int getTotalSize() { 
         return totalSize; 
+    }
+
+    public String getReadingTime() {
+        return readingTime;
     }
 }
